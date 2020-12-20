a58ed485c9c2011ff3c61bce8743fba7234d4ced
Anton Ryzhov <anton.ryzhov@vertoanalytics.com>
2020-12-20 19:31:07 +0200

    Repro code for avalonia issue 5194

---
+++ Avalonia-Issue-5194/AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>()
+        {
+            new Item()
+        };
+    }
+}

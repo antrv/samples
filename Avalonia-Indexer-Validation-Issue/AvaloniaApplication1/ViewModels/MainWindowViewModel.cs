7f50cf6c56e1001f003e0967dc9f1c0e61d1c7dd
Anton Ryzhov <anton.ryzhov@vertoanalytics.com>
2020-12-24 17:25:41 +0200

    Avalonia indexer validation issue

---
+++ Avalonia-Indexer-Validation-Issue/AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Person> Items { get; } = new ObservableCollection<Person>()
+        {
+            new Person()
+            {
+                Name = "Alice",
+                Age = 23
+            },
+            new Person()
+            {
+                Name = "Bob",
+                Age = 44
+            }
+        };
+    }
+}
---
+++ Avalonia-Indexer-Validation-Issue/AvaloniaApplication1/ViewModels/Person.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Avalonia.Data;
+using ReactiveUI;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class AdditionalData : ReactiveObject
+    {
+        private readonly string[] _data =
+            Enumerable.Range(0, 10)
+                .Select(n => $"Additional {n}").ToArray();
+
+        public string this[int index]
+        {
+            get => _data[index];
+            set
+            {
+                if (_data[index] != value)
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        throw new DataValidationException("Additional value cannot be null or empty");
+                    }
+
+                    this.RaiseAndSetIfChanged(ref _data[index], value, "Item[]");
+                }
+            }
+        }
+    }
+
+    public class Person : ReactiveObject
+    {
+        private readonly AdditionalData _additionalData = new AdditionalData();
+        private string _name;
+        private int _age;
+
+        public AdditionalData AdditionalData => _additionalData;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        throw new DataValidationException("Name cannot be null or empty");
+                    }
+
+                    this.RaiseAndSetIfChanged(ref _name, value);
+                }
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => this.RaiseAndSetIfChanged(ref _age, value);
+        }
+    }
+}
